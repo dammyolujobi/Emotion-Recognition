f61080c44874c59b688ea6d33da7dc5cc99688ac
dammyjones <daolabmovies@gmail.com>
2025-11-03 00:11:52 +0100

    Fix: Add port specification to uvicorn.run for clarity

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ async def upload_image(file:UploadFile = File(...)):
     return f"Predicted emotion: {label}"
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app",reload=True,host="0.0.0.0")
+    uvicorn.run(app="main:app",reload=True,host="0.0.0.0",port=8000)
